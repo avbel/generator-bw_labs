230253508955b8431247940ce42ce135c3738bca
Andrey Belchikov <belchikovs@gmail.com>
2014-05-22 23:25:33 -0700

    Added ability to generate root controller

--- controller/index.js
+++ controller/index.js
@@ -1,12 +1,20 @@
 'use strict';
 var util = require('util');
+var fs = require('fs');
 var yeoman = require('yeoman-generator');
+var config = require('../lib/config');
 
 
 var ControllerGenerator = yeoman.generators.NamedBase.extend({
   files: function () {
+    var cfg = config.loadAppConfigFile.call(this);
+    this.viewsEnabled = (cfg.views && cfg.views.enabled !== false);
     this.mkdir('app/controllers');
-    this.copy('controller.js', 'app/controllers/' + this._.camelize(this._.underscored(this.name)) + '.js');
+    this.copy('controller.js', 'app/controllers/' + ((this.name == "__root")?this.name:this._.camelize(this._.underscored(this.name))) + '.js');
+    if(this.viewsEnabled && this.name == "__root"){
+      this.mkdir('app/views');
+      this.copy('index.jade', 'app/views/index.jade');
+    }
   }
 });
 
--- controller/templates/controller.js
+++ controller/templates/controller.js
@@ -2,7 +2,10 @@
 module.exports = function*(app){
   return app.Controller.extend({
     actions: {
-      //TODO add actions
+      //TODO: add actions
+      <% if(name=='__root'){%>index: function*(){
+        this.body = <%= (viewsEnabled)?"yield this.render(\"index\")":"\"\"" %>;
+      }<% } %>
     }
   });
 };
